135dd2aa36ea0ceebdc2354d7e3f8ece67bcc484
veenadhar <v.v.dharmadhikari@gmail.com>
2023-01-31 00:17:11 +0530

    first commit

---
+++ src/main/java/com/java/test/Git_Demo_Project1/App.java
@@ -0,0 +1,15 @@
+package com.java.test.Git_Demo_Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!2" );
+        System.out.println( "Hello World!3" );
+    }
+}
